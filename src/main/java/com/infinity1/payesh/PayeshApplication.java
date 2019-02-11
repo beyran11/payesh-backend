5190aa17a3eb953500cb7d30de1b3726113bc4ad
Hamzeh Beiranvand <beyran11@gmail.com>
2019-02-11 20:53:07 +0330

    create project

---
+++ src/main/java/com/infinity1/payesh/PayeshApplication.java
@@ -0,0 +1,14 @@
+package com.infinity1.payesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayeshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayeshApplication.class, args);
+    }
+
+}
+
