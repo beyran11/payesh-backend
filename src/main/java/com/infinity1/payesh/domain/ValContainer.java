ca1ff42905bf8b4686abedda01b7636074a05e9d
Hamzeh Beiranvand <beyran11@gmail.com>
2019-02-12 22:34:59 +0330

    creat a post API

---
+++ src/main/java/com/infinity1/payesh/domain/ValContainer.java
@@ -0,0 +1,14 @@
+package com.infinity1.payesh.domain;
+
+public class ValContainer {
+
+    private double val;
+
+    public double getVal() {
+        return val;
+    }
+
+    public void setVal(double val) {
+        this.val = val;
+    }
+}
--- src/main/java/com/infinity1/payesh/resource/UserController.java
+++ src/main/java/com/infinity1/payesh/resource/UserController.java
@@ -1,8 +1,7 @@
 package com.infinity1.payesh.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.infinity1.payesh.domain.ValContainer;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -23,6 +22,16 @@ public class UserController {
         return Math.sin(Math.PI/2);
     }
 
+    @PostMapping("/post")
+    private String getName(@RequestBody String name) {
+        System.out.println(name);
+        return name;
+    }
 
+    @PostMapping("/post/trigon")
+    private double getVal(@RequestBody ValContainer valContainer) {
+        System.out.println(valContainer.getVal());
+        return Math.sin(valContainer.getVal()*Math.PI/180);
+    }
 }
 
